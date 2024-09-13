fa86f3d3293690d39b9ad62a360e2f97a4c0bc7c
Johan Stenstam <johani@johani.org>
2024-09-13 16:50:03 +0200

    * some cleanup of the status reporting infrastructure

--- logger.go
+++ logger.go
@@ -1,3 +1,17 @@
+// Copyright 2024 Johan Stenstam, johan.stenstam@internetstiftelsen.se
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -29,15 +43,15 @@ func (l *Logger) LogStatus(edgeId, edgeComponent string, status tapir.TapirFunct
 	l.log.Printf("Status update from TAPIR Edge (id: %s, component: %s)", edgeId, edgeComponent)
 	for _, comp := range status.ComponentStatus {
 		switch comp.Status {
-		case "fail":
+		case tapir.StatusFail:
 			l.log.Printf("TAPIR-POP %s Component: %s, Status: %s, Message: %s, Time of failure: %s",
-				status.FunctionID, comp.Component, comp.Status, comp.Msg, comp.LastFail.Format(time.RFC3339))
-		case "warn":
+				status.FunctionID, comp.Component, tapir.StatusToString[comp.Status], comp.Msg, comp.LastFail.Format(time.RFC3339))
+		case tapir.StatusWarn:
 			l.log.Printf("TAPIR-POP %s: Component: %s, Status: %s, Message: %s, Time of warning: %s",
-				status.FunctionID, comp.Component, comp.Status, comp.Msg, comp.LastWarn.Format(time.RFC3339))
-		case "success":
+				status.FunctionID, comp.Component, tapir.StatusToString[comp.Status], comp.Msg, comp.LastWarn.Format(time.RFC3339))
+		case tapir.StatusOK:
 			l.log.Printf("TAPIR-POP %s Component: %s, Status: %s, Message: %s, Time of success: %s",
-				status.FunctionID, comp.Component, comp.Status, comp.Msg, comp.LastSuccess.Format(time.RFC3339))
+				status.FunctionID, comp.Component, tapir.StatusToString[comp.Status], comp.Msg, comp.LastSuccess.Format(time.RFC3339))
 		}
 	}
 }
